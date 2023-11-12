0d02265da69e1b1552e8bdb73245615162dc0255
Martin Szabo <martin.szabo333@gmail.com>
2023-11-12 13:45:36 +0100

    Fixing sendto datatype issue

--- modes/Sender.py
+++ modes/Sender.py
@@ -1,6 +1,7 @@
 import socket as s
 from utils.Constants import DEFAULT_PORT
 from packet.Packet import Packet
+from utils.Coder import encode_str_to_bytes
 
 
 class Sender:
@@ -10,4 +11,6 @@ class Sender:
         self.server = (ip, port)
 
     def send_packet(self, data: Packet):
-        self.socket.sendto(data.encode(), self.server)
+        encoded_data_string = data.encode()
+        encoded_data_bytes = encode_str_to_bytes(encoded_data_string)
+        self.socket.sendto(encoded_data_bytes, self.server)
--- packet/Packet.py
+++ packet/Packet.py
@@ -1,6 +1,6 @@
 from data.Data import Data
 from packet.Flags import Flags
-from utils.Coder import encode_int_to_hex, decode_int_from_hex
+from utils.Coder import encode_int_to_hex, decode_int_from_hex, decode_str_from_bytes
 from utils.Constants import SEQ_SIZE, CRC_SIZE
 
 
@@ -17,6 +17,9 @@ class Packet:
         return self.flags.encode() + encoded_seq + encoded_crc + self.data
 
     def decode(self, data):
+        if isinstance(data, bytes):
+            data = decode_str_from_bytes(data)
+
         self.flags = Flags()
         self.flags.decode(data[0:4])
         self.seq = decode_int_from_hex(data[4:12])
