f14ec7bd46c6762b0ac6ff52d6ecd462415c6863
Martin Szabo <martin.szabo333@gmail.com>
2023-11-12 11:42:16 +0100

    Establishing connection

--- modes/Receiver.py
+++ modes/Receiver.py
@@ -1,3 +1,18 @@
+import socket as s
+
+
 class Receiver:
-    def __init__(self):
-        pass
+    def __init__(self, port: int = 3333, ip: str = "0.0.0.0"):
+        self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+        self.socket.bind((ip, port))
+        self.socket.listen(1)
+
+        print("Waiting for connection...")
+
+        while True:
+            conn, addr = self.socket.accept()
+            print("Connected from", addr)
+
+            # TODO:: Wait for SYN packet to start transmitting data. If SYN is received. Send ACK, etc...
+
+            conn.close()
--- modes/Sender.py
+++ modes/Sender.py
@@ -1,3 +1,9 @@
+import socket as s
+
+
 class Sender:
-    def __init__(self):
-        pass
+    def __init__(self, ip: str, port: int = 3333):
+        # TODO:: Handle data via assembler, connect to server then start sending sequence
+
+        self.socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+        self.socket.connect((ip, port))
