684a81428d3d25e7bbae662b9c78031166ecce64
Martin Szabo <martin.szabo333@gmail.com>
2023-11-17 17:13:37 +0100

    Fixing fragmentation bug

--- utils/Constants.py
+++ utils/Constants.py
@@ -14,14 +14,16 @@ ETHERNET_HEADER_LENGTH = 14
 MIN_IP_HEADER_LENGTH = 20
 MAX_IP_HEADER_LENGTH = 60
 UDP_HEADER_LENGTH = 8
-UNUSED_BYTES = 6  # FLAGS is only 2 bytes which leaves 2 bytes free, SEQ is only 3 bytes which leaves 1 byte free.
 IP_HEADER_LENGTH = MIN_IP_HEADER_LENGTH  # We'll count with minimum IP header length
+
+# TODO:: Find out why this changed from 6 to 5 (Due to the INFO packet?)
+UNUSED_BYTES = 5  # FLAGS is only 2 bytes which leaves 2 bytes free, SEQ is only 3 bytes which leaves 1 byte free.
 
 ###############################################
 # Calculated constants
 ###############################################
 
-MAX_PACKET_SIZE = MTU - ETHERNET_HEADER_LENGTH - IP_HEADER_LENGTH + UNUSED_BYTES  # Bytes
+MAX_PACKET_SIZE = MTU - ETHERNET_HEADER_LENGTH - IP_HEADER_LENGTH - UDP_HEADER_LENGTH + UNUSED_BYTES  # Bytes
 MAX_PAYLOAD_SIZE = MAX_PACKET_SIZE - SEQ_SIZE - CRC_SIZE - FLAGS_SIZE  # Bytes
 
 # Sizes for ranges in bytes array
