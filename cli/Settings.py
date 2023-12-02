4769a9ef498282de7d9ab5b6b015c016aa3b4653
Martin Szabo <martin.szabo333@gmail.com>
2023-12-02 18:19:25 +0100

    Fixing bugs and removing NACK mult

--- cli/Settings.py
+++ cli/Settings.py
@@ -2,7 +2,7 @@ from ast import literal_eval
 
 from cli.Menu import Menu
 from utils.Constants import DEFAULT_PORT, MAX_SEGMENT_SIZE, SENDER_BAD_PACKETS_SEQ, SENDER_BAD_PACKETS_ATTEMPTS, \
-    RESEND_ATTEMPTS, NACK_RESPONSE_MULTIPLIER
+    RESEND_ATTEMPTS, DEFAULT_SERVER_IP
 from utils.Utils import is_valid_ipv4, get_integer_safely, get_list_safely, get_string_safely, \
     get_downloads_folder, select_folder, convert_bytes_to_str, convert_str_to_bytes, is_port_in_use
 
@@ -17,7 +17,6 @@ class Settings:
         bad_packets_seq: list = SENDER_BAD_PACKETS_SEQ,
         bad_packets_attempts: int = SENDER_BAD_PACKETS_ATTEMPTS,
         segment_size: int = MAX_SEGMENT_SIZE,
-        nack_response_multiplier: int = NACK_RESPONSE_MULTIPLIER,
         downloads_dir: str = None
     ):
         self.ip = ip
@@ -26,7 +25,6 @@ class Settings:
         self.bad_packets_seq = bad_packets_seq
         self.bad_packets_attempts = bad_packets_attempts
         self.segment_size = segment_size
-        self.nack_response_multiplier = nack_response_multiplier
         self.downloads_dir = get_downloads_folder() if downloads_dir is None else downloads_dir
         self._initialized = False if ip is None else True
 
@@ -37,11 +35,14 @@ class Settings:
         self.get_segment_size()
 
     def get_ip(self):
-        self.ip = get_string_safely("Please enter IP address: ", DEFAULT_PORT, lambda x: is_valid_ipv4(x),
+        self.ip = get_string_safely("Please enter IP address: ", DEFAULT_SERVER_IP, lambda x: is_valid_ipv4(x),
                                     "Invalid IP address")
         self._initialized = True
 
     def get_port(self):
+        if self._initialized:
+            return
+
         self.port = get_integer_safely(
             "Please enter port number: ", DEFAULT_PORT,
             lambda x: not is_port_in_use(x, self.ip),
@@ -65,10 +66,6 @@ class Settings:
     def get_packet_resend_attempts(self):
         self.packet_resend_attempts = get_integer_safely("Please enter packet resend attempts: ", RESEND_ATTEMPTS)
 
-    def get_nack_response_multiplier(self):
-        self.nack_response_multiplier = get_integer_safely("Please enter nack response multiplier: ",
-                                                           NACK_RESPONSE_MULTIPLIER)
-
     def get_downloads_folder(self):
         self.downloads_dir = select_folder("Please select downloads folder")
 
@@ -86,7 +83,6 @@ class Settings:
         modify_menu.add_option("Bad packets SEQ numbers", lambda: self.get_bad_packets_seq())
         modify_menu.add_option("Bad packets resend attempts", lambda: self.get_bad_packets_attempts())
         modify_menu.add_option("Packet resend attempts", lambda: self.get_packet_resend_attempts())
-        modify_menu.add_option("NACK response multiplier", lambda: self.get_nack_response_multiplier())
         modify_menu.add_option("Download folder", lambda: self.get_downloads_folder())
         modify_menu.display()
 
@@ -97,7 +93,6 @@ class Settings:
                f"Bad packets seq: {self.bad_packets_seq}\n" \
                f"Bad packets send attempts: {self.bad_packets_attempts}\n" \
                f"Packet resend attempts: {self.packet_resend_attempts}\n" \
-               f"NACK response multiplier: {self.nack_response_multiplier}\n" \
                f"Downloads folder: {self.downloads_dir}"
 
     def encode(self):
@@ -108,7 +103,6 @@ class Settings:
             + str(self.bad_packets_seq) + ';'
             + str(self.bad_packets_attempts) + ';'
             + str(self.packet_resend_attempts) + ';'
-            + str(self.nack_response_multiplier) + ';'
         )
         return convert_str_to_bytes(_str)
 
@@ -120,5 +114,4 @@ class Settings:
         self.bad_packets_seq = literal_eval(data[3])
         self.bad_packets_attempts = int(data[4])
         self.packet_resend_attempts = int(data[5])
-        self.nack_response_multiplier = int(data[6])
         return self
